81a5c7f3de98f478d873db77822a437a40f59ab4
naudc <naudc99@gmail.com>
2025-02-17 16:48:21 +0000

    add api-rdeco

---
+++ src/main/java/es/ndc/api_rdeco/ApiRdecoApplication.java
@@ -0,0 +1,13 @@
+package es.ndc.api_rdeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRdecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRdecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ndc/api_rdeco/ApiRdecoApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ndc.api_rdeco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRdecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
